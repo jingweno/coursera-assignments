1c33058b18186112f5c2dc1b8e9e336551f2655f
Jingwen Owen Ou <jingweno@gmail.com>
2012-09-27 12:02:58 -0700

    Implement PascalSuite

--- progfun/recfun/src/main/scala/recfun/Main.scala
+++ progfun/recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,46 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+
+    def recursivePascal(c: Int, r: Int): Int = {
+      if (!validInput(c, r)) {
+        throw new IllegalArgumentException("Input is invalid")
+      }
+
+      if (isEdge(c, r)) {
+        1
+      } else {
+        // (1,2) -> (0, 1) + (1, 1)
+        // (1,3) -> (0, 2) + (1, 2)
+        recursivePascal(c - 1, r - 1) + recursivePascal(c, r - 1)
+      }
+    }
+
+    def validInput(c: Int, r: Int): Boolean = {
+      if (c <= r) {
+        true
+      } else {
+        false
+      }
+    }
+
+    def isEdge(c: Int, r: Int): Boolean = {
+      // 1 (0, 0)
+      // 2 (0, 1) (1,1)
+      // 3 (0, 2) (2,2)
+      // 4 (0, 3) (3, 3)
+
+      if (c == 0 || c == r) {
+        true
+      } else {
+        false
+      }
+
+    }
+
+    recursivePascal(c, r)
+  }
 
   /**
    * Exercise 2
--- progfun/recfun/src/test/scala/recfun/PascalSuite.scala
+++ progfun/recfun/src/test/scala/recfun/PascalSuite.scala
@@ -8,15 +8,37 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class PascalSuite extends FunSuite {
   import Main.pascal
+  test("col=0,row=0") {
+    assert(pascal(0, 0) === 1)
+  }
+
   test("col=0,row=2") {
-    assert(pascal(0,2) === 1)
+    assert(pascal(0, 2) === 1)
+  }
+
+  test("col=2,row=2") {
+    assert(pascal(0, 2) === 1)
   }
 
   test("col=1,row=2") {
-    assert(pascal(1,2) === 2)
+    assert(pascal(1, 2) === 2)
   }
 
   test("col=1,row=3") {
-    assert(pascal(1,3) === 3)
+    assert(pascal(1, 3) === 3)
+  }
+
+  test("col=2,row=4") {
+    assert(pascal(2, 4) === 6)
+  }
+
+  test("col=3,row=4") {
+    assert(pascal(3, 4) === 4)
+  }
+
+  test("col=5,row=4") {
+    intercept[IllegalArgumentException] {
+      pascal(5, 4)
+    }
   }
 }
