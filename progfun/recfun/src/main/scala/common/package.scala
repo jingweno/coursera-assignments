01c4fad682b482e02d81a073224d60663ada2577
Jingwen Owen Ou <jingweno@gmail.com>
2012-09-27 08:49:17 -0700

    Check in empty project for recfun

--- progfun/recfun/src/main/scala/common/package.scala
+++ progfun/recfun/src/main/scala/common/package.scala
@@ -1,6 +1,5 @@
 package object common {
-  /**
-   * `???` can be used for marking methods that remain to be implemented.
+  /** `???` can be used for marking methods that remain to be implemented.
    *  @throws  An `Error`
    */
   def ??? : Nothing = throw new Error("an implementation is missing")
